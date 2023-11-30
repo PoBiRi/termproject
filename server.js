c18022862e2e82a9c0cef8f0a18eba5be5e7edb9
PoBiRi <124334953+PoBiRi@users.noreply.github.com>
2023-11-30 13:44:16 +0900

    Update server.js

--- server.js
+++ server.js
@@ -17,13 +17,6 @@ app.engine('html', require('ejs').renderFile);
 app.use(express.static(__dirname + '/views'));
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // 또는 특정 도메인
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
 app.get('/', (req, res) =>{
     res.render('index');
 });
